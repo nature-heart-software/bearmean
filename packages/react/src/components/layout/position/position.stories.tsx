27d8be88018d1a6cff412fbac71ef23cadc07b83
Wurielle <noahfenghom@gmail.com>
2024-06-03 09:50:49 +0200

    feat: update position story

--- packages/react/src/components/layout/position/position.stories.tsx
+++ packages/react/src/components/layout/position/position.stories.tsx
@@ -8,22 +8,24 @@ const meta = {
 
 export default meta
 
-const Template: StoryFn<typeof Position> = (args) => <Position {...args} bc={'slate.200'} bw={1} p={'8'} display={'inline-flex'}>
-    {/* Corners */}
-    <Position absolute top left translate={'-50% -50%'} br={'full'} p={'3'} bc={'slate.200'} bw={1} />
-    <Position absolute top right translate={'50% -50%'} br={'full'} p={'3'} bc={'slate.200'} bw={1} />
-    <Position absolute bottom left translate={'-50% 50%'} br={'full'} p={'3'} bc={'slate.200'} bw={1} />
-    <Position absolute bottom right translate={'50% 50%'} br={'full'} p={'3'} bc={'slate.200'} bw={1} />
+const Template: StoryFn<typeof Position> = (args) => (
+    <Position {...args} bc={'slate.200'} bw={1} p={'8'} display={'inline-flex'}>
+        {/* Corners */}
+        <Position absolute top left translate={'-50% -50%'} br={'full'} p={'3'} bc={'slate.200'} bw={1} />
+        <Position absolute top right translate={'50% -50%'} br={'full'} p={'3'} bc={'slate.200'} bw={1} />
+        <Position absolute bottom left translate={'-50% 50%'} br={'full'} p={'3'} bc={'slate.200'} bw={1} />
+        <Position absolute bottom right translate={'50% 50%'} br={'full'} p={'3'} bc={'slate.200'} bw={1} />
 
-    {/* Circle */}
-    <Position absolute inset br={'full'} p={'3'} bc={'slate.200'} bw={1} />
+        {/* Circle */}
+        <Position absolute inset br={'full'} p={'3'} bc={'slate.200'} bw={1} />
 
-    {/* Bordered Box */}
-    <Position absolute top={'50%'} left={'50%'} translate={'-25% -50%'} p={'5'} bc={'slate.200'} bw={1} z={1} />
+        {/* Bordered Box */}
+        <Position absolute top={'50%'} left={'50%'} translate={'-25% -50%'} p={'5'} bc={'slate.200'} bw={1} z={1} rotate={'45deg'} />
 
-    {/* Background Box */}
-    <Position absolute top={'50%'} left={'50%'} translate={'-75% -50%'} p={'5'} bg={'slate.100'} />
-</Position>
+        {/* Background Box */}
+        <Position absolute top={'50%'} left={'50%'} translate={'-75% -50%'} p={'5'} bg={'slate.100'} rotate={'45deg'} />
+    </Position>
+)
 
 export const Default = Template.bind({})
 Default.args = {}
