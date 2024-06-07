513ad615639d8162e1e315248f7d2253ba88b372
Wurielle <noahfenghom@gmail.com>
2024-06-07 14:15:01 +0200

    feat: wip improve transform props

--- packages/react/src/components/layout/transform/transform.shared.ts
+++ packages/react/src/components/layout/transform/transform.shared.ts
@@ -2,14 +2,30 @@ import { HTMLAttributes } from 'react'
 import { Properties } from 'csstype'
 import { defineProps } from '@/utils/component'
 import { boxPropsDefinition } from '@/components/layout/box'
+import { Spacing } from '@/tokens'
 
 export const transformPropsDefinition = defineProps(({ optional }) => ({
     ...boxPropsDefinition,
     transform: optional<Properties['transform']>(),
     transformOrigin: optional<Properties['transformOrigin']>(),
-    translate: optional<Properties['translate']>(),
-    rotate: optional<Properties['rotate']>(),
-    scale: optional<Properties['scale']>(),
+    translate: optional<(Spacing | Properties['translate'])[]>(),
+    translateX: optional<Spacing | Properties['translate']>(),
+    translateY: optional<Spacing | Properties['translate']>(),
+    translateZ: optional<Spacing | Properties['translate']>(),
+    translate3d: optional<(Spacing | Properties['translate'])[]>(),
+    scale: optional<(Spacing | Properties['scale'])[]>(),
+    scaleX: optional<Properties['scale']>(),
+    scaleY: optional<Properties['scale']>(),
+    scaleZ: optional<Properties['scale']>(),
+    scale3d: optional<(Spacing | Properties['scale'])[]>(),
+    skew: optional<string[]>(),
+    skewX: optional<string>(),
+    skewY: optional<string>(),
+    rotate: optional<(Spacing | Properties['rotate'])[]>(),
+    rotateX: optional<Properties['rotate']>(),
+    rotateY: optional<Properties['rotate']>(),
+    rotateZ: optional<Properties['rotate']>(),
+    rotate3d: optional<(Spacing | Properties['rotate'])[]>(),
     perspective: optional<Properties['perspective']>(),
 }))
 
--- packages/react/src/components/layout/transform/transform.styled.tsx
+++ packages/react/src/components/layout/transform/transform.styled.tsx
@@ -2,28 +2,67 @@ import styled from '@emotion/styled'
 import { TransformPropsDefinition } from './transform.shared'
 import { StBox } from '@/components/layout/box'
 import isUndefined from 'lodash/isUndefined'
+import { getRawValue } from '@/utils'
+import { spacing as _spacing } from '@/tokens'
 
 export const StTransform = styled(StBox, {
     shouldForwardProp: (prop) => !['styled', 'as'].includes(prop),
 })<{
     styled: TransformPropsDefinition
-}>(({ styled: { transform, transformOrigin, perspective, translate, rotate, scale } }) => [
-    transform && {
-        transform,
-    },
-    transformOrigin && {
+}>(({
+    theme: { spacing = _spacing },
+    styled: {
+        transform: transformProp,
         transformOrigin,
-    },
-    perspective && {
         perspective,
-    },
-    translate && {
         translate,
-    },
-    rotate && {
+        translateX,
+        translateY,
+        translateZ,
+        translate3d,
         rotate,
-    },
-    !isUndefined(scale) && {
         scale,
+        scaleX,
+        scaleY,
+        scaleZ,
+        scale3d,
     },
-])
+}) => {
+    const transform = [
+        transformProp && `${getRawValue(transformProp, spacing)}`,
+        translate &&
+            `translate(${translate
+                .map((value) => value && getRawValue(value, spacing))
+                .filter(Boolean)
+                .join(', ')})`,
+        translateX && `translateX(${getRawValue(translateX, spacing)})`,
+        translateY && `translateY(${getRawValue(translateY, spacing)})`,
+        translateZ && `translateZ(${getRawValue(translateZ, spacing)})`,
+        translate3d && `translate3d(${getRawValue(translate3d, spacing)})`,
+        scale && `scale(${getRawValue(scale, spacing)})`,
+        scaleX && `scaleX(${getRawValue(scaleX, spacing)})`,
+        scaleY && `scaleY(${getRawValue(scaleY, spacing)})`,
+        scaleZ && `scaleZ(${getRawValue(scaleZ, spacing)})`,
+        scale3d && `scale3d(${getRawValue(scale3d, spacing)})`,
+    ].join(' ')
+    return [
+        transform && {
+            transform,
+        },
+        transformOrigin && {
+            transformOrigin,
+        },
+        perspective && {
+            perspective,
+        },
+        translate && {
+            translate,
+        },
+        rotate && {
+            rotate,
+        },
+        !isUndefined(scale) && {
+            scale,
+        },
+    ]
+})
