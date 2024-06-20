e0dce0b89a92bc2650b6cbd00b4480727decad94
Wurielle <noahfenghom@gmail.com>
2024-06-21 00:03:08 +0200

    feat: wip add defaults types

--- packages/react/src/utils/component.ts
+++ packages/react/src/utils/component.ts
@@ -10,8 +10,27 @@ export type UndefinedProperties<T> = {
 export type ToOptional<T> = Partial<Pick<T, UndefinedProperties<T>>> & Pick<T, Exclude<keyof T, UndefinedProperties<T>>>
 
 type NonUndefined<T> = T extends undefined ? never : T
-const optional = <T>(defaultValue?: T) => defaultValue as T | undefined
-const required = <T>(defaultValue?: T) => defaultValue as NonUndefined<T>
+
+type DefineOptional = {
+    <T>(): T | undefined | '_OPTIONAL'
+    <T>(defaultValue: T): T | undefined | '_OPTIONAL' | '_HAS_DEFAULT_VALUE'
+}
+
+type DefineRequired = {
+    <T>(): NonUndefined<T> | '_REQUIRED'
+    <T>(defaultValue: T): NonUndefined<T> | '_REQUIRED' | '_HAS_DEFAULT_VALUE'
+}
+
+const optional: DefineOptional = (defaultValue?) => {
+    return defaultValue
+}
+
+const required: DefineRequired = (defaultValue?) => {
+    return defaultValue
+}
+
+// const optional = <T>(defaultValue?: T) => defaultValue as T | undefined
+// const required = <T>(defaultValue?: T) => defaultValue as NonUndefined<T>
 const responsive = <P extends Record<string, unknown>>(props: P) => {
     return {
         ...props,
@@ -36,8 +55,8 @@ type Props = {
 }
 
 type DefinitionUtils = {
-    optional: <T>(defaultValue?: T) => T | undefined
-    required: <T>(defaultValue?: T) => NonUndefined<T>
+    optional: DefineOptional
+    required: DefineRequired
     responsive: <P extends Record<string, unknown>>(props: P) => P & { [key in `${Screen}${Capitalize<Extract<keyof P, string>>}`]: P[keyof P] }
 }
 type Definition<P extends Props> = (DefinitionUtils: DefinitionUtils) => P
@@ -46,7 +65,7 @@ export const defineProps = <P extends Props>(definition: Definition<P>) =>
         optional,
         required,
         responsive,
-    }) as ToOptional<P>
+    })
 
 export const useDefinitionProps = <P extends object, D extends object>(props: P, propsDefinition: D, overrideProps?: D): [D, Omit<P, keyof D>] => {
     const propsDefinitionWithVariants = { ...propsDefinition, ...overrideProps } as D
@@ -58,3 +77,34 @@ export const useDefinitionProps = <P extends object, D extends object>(props: P,
 export const useVariantProps = <V extends Record<string, unknown>, CV extends keyof V>(variants: V, currentVariant?: CV) => {
     return useMemo(() => (currentVariant ? variants[currentVariant] : {}), [currentVariant, variants])
 }
+
+type RemoveUnionItem<T, U> = T extends U ? never : T
+
+type RemoveItemFromAllKeys<T, U> = {
+    [K in keyof T]: RemoveUnionItem<T[K], U>
+}
+
+type RemoveUndefinedIfHasDefaultValue<T> = '_HAS_DEFAULT_VALUE' extends T ? NonNullable<T> : T
+
+type RemoveUndefinedFromAllKeys<T> = {
+    [K in keyof T]: RemoveUndefinedIfHasDefaultValue<T[K]>
+}
+
+type DefinitionProps<P> = ToOptional<RemoveItemFromAllKeys<P, '_OPTIONAL' | '_REQUIRED' | '_HAS_DEFAULT_VALUE'>>
+type DefinitionPropsWithDefaults<P> = RemoveItemFromAllKeys<RemoveUndefinedFromAllKeys<P>, '_OPTIONAL' | '_REQUIRED' | '_HAS_DEFAULT_VALUE'>
+
+// TODO: doesn't work with string, wrap everything with new methods or types, check if it works well with responsive
+type Vals = 'a' | 'b'
+
+const someProps = defineProps(({ optional, required }) => ({
+    optionalValue: optional<Vals>('a'),
+    requiredValue: required<Vals>(),
+}))
+
+const { optionalValue, requiredValue } = someProps as ToOptional<typeof someProps>
+const { optionalValue, requiredValue } = someProps as DefinitionProps<typeof someProps>
+const { optionalValue, requiredValue } = someProps as DefinitionPropsWithDefaults<typeof someProps>
+
+function zea(props: ToOptional<RemoveItemFromAllKeys<typeof someProps, '_OPTIONAL' | '_REQUIRED' | '_HAS_DEFAULT_VALUE'>>) {}
+
+zea({ requiredValue: 'a' })
