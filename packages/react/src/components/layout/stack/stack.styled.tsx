7d4194c69924d2a08e58aa00c615c21a28a5d46e
Wurielle <noahfenghom@gmail.com>
2025-01-03 17:01:05 +0100

    feat: add responsive props

--- packages/react/src/components/layout/stack/stack.styled.tsx
+++ packages/react/src/components/layout/stack/stack.styled.tsx
@@ -1,8 +1,9 @@
 import styled from '@emotion/styled'
 import { StackPropsWithDefaults } from './stack.shared'
-import { getRemValue, StyledProps } from '@/utils/css-in-js'
+import { defineMixins, getRemValue, StyledProps } from '@/utils/css-in-js'
 import { spacing as _spacing } from '@/tokens'
 import { StBox } from '@/components/layout/box'
+import isUndefined from 'lodash/isUndefined'
 
 const POSITIONS = {
     top: 'flex-start',
@@ -19,18 +20,42 @@ const POSITIONS = {
 export const StStack = styled(StBox)<StyledProps<StackPropsWithDefaults>>((context) => {
     const {
         theme: { spacing = _spacing },
-        styled: { justify, align, gap },
     } = context
-    return {
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: align in POSITIONS ? POSITIONS[align as keyof typeof POSITIONS] : align,
-        alignItems: justify in POSITIONS ? POSITIONS[justify as keyof typeof POSITIONS] : justify,
-        gap: getRemValue(gap, spacing),
-        '& > *': {
-            minWidth: 0,
-            minHeight: 0,
-            flexGrow: align === 'stretch' ? 1 : undefined,
+    const { getResponsive } = defineMixins(context)
+    return [
+        {
+            display: 'flex',
+            flexDirection: 'column',
         },
-    }
+        getResponsive(
+            'align',
+            (align) =>
+                !isUndefined(align) && {
+                    justifyContent: align in POSITIONS ? POSITIONS[align as keyof typeof POSITIONS] : align,
+                    '& > *': {
+                        flexGrow: align === 'stretch' ? 1 : undefined,
+                    },
+                }
+        ),
+        getResponsive(
+            'justify',
+            (justify) =>
+                !isUndefined(justify) && {
+                    alignItems: justify in POSITIONS ? POSITIONS[justify as keyof typeof POSITIONS] : justify,
+                }
+        ),
+        getResponsive(
+            'gap',
+            (gap) =>
+                !isUndefined(gap) && {
+                    gap: getRemValue(gap, spacing),
+                }
+        ),
+        {
+            '& > *': {
+                minWidth: 0,
+                minHeight: 0,
+            },
+        },
+    ]
 })
