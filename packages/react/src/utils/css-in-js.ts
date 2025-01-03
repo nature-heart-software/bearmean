a594a2c2a3f69d39e5353b573e4498041887b11a
Wurielle <noahfenghom@gmail.com>
2025-01-03 16:49:10 +0100

    feat: add respoinsive props

--- packages/react/src/utils/css-in-js.ts
+++ packages/react/src/utils/css-in-js.ts
@@ -3,6 +3,7 @@ import { Screen, Screens, screens as _screens } from '@/tokens'
 import { InterpolationPrimitive } from '@emotion/serialize'
 import get from 'lodash/get'
 import isUndefined from 'lodash/isUndefined'
+import { CSSObject } from '@emotion/styled'
 
 export type StyledProps<P extends object> = {
     styled: P
@@ -33,31 +34,39 @@ export const defineMixins = <
 >(
     context: Context
 ) => {
+    function getScreenBreakpointValue({ value, margin }: Screens[keyof Screens]) {
+        return value + 2 * margin
+    }
+
     const getResponsive = <
         Prop extends keyof Context['styled'],
         Callback extends (value: Context['styled'][Prop], screen: Screen | null, prop: Prop) => InterpolationPrimitive,
     >(
         prop: Prop,
-        callback: Callback
+        callback: Callback,
+        currentScreen?: Screen | null // allows for nested getResponsive()
     ) => {
         const screens = context.theme?.screens || _screens
         const props = context.styled || {}
         return [
-            callback(props[prop as keyof typeof props] as Context['styled'][Prop], null, prop),
-            ...(Object.entries(screens) as [Screen, Screens[keyof Screens]][]).map(([screen, { value, margin }]) => {
+            !currentScreen && callback(props[prop as keyof typeof props] as Context['styled'][Prop], null, prop),
+            ...(Object.entries(screens) as [Screen, Screens[keyof Screens]][]).map(([screen, screenValue]) => {
+                const screenTotalSize = getScreenBreakpointValue(screenValue)
+                if (currentScreen && getScreenBreakpointValue(screens[currentScreen]) > screenTotalSize) return null
                 const responsiveProp = `${screen}${(prop as string)[0].toUpperCase()}${(prop as string).slice(1)}` as Prop
                 if (isUndefined(props[responsiveProp as keyof typeof props])) return null
                 const responsiveValue = props[responsiveProp as keyof typeof props] as Context['styled'][Prop]
                 const result = callback(responsiveValue, screen, responsiveProp)
                 return (
                     result && {
-                        [`@media (min-width: ${rem(value + 2 * margin)})`]: result,
+                        [`@media (min-width: ${rem(screenTotalSize)})`]: result,
                     }
                 )
             }),
-        ]
+        ].filter(Boolean) as CSSObject[]
     }
     return {
         getResponsive,
+        getScreenBreakpointValue,
     }
 }
