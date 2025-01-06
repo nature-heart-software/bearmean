1e80a84fb8ff5538af26fedff36bf22a4e4c92bd
Wurielle <noahfenghom@gmail.com>
2025-01-06 16:34:23 +0100

    feat: add responsive props

--- packages/react/src/components/layout/position/position.shared.ts
+++ packages/react/src/components/layout/position/position.shared.ts
@@ -4,20 +4,22 @@ import { defineProps, PropsDefinition, PropsDefinitionWithDefaults } from '@/uti
 import { Level, Spacing } from '@/tokens'
 import { transformPropsDefinition } from '@/components/layout/transform'
 
-export const positionPropsDefinition = defineProps(({ optional }) => ({
+export const positionPropsDefinition = defineProps(({ responsive, optional }) => ({
     ...transformPropsDefinition,
-    position: optional<Properties['position']>('relative'),
-    relative: optional<boolean>(),
-    absolute: optional<boolean>(),
-    fixed: optional<boolean>(),
-    sticky: optional<boolean>(),
-    static: optional<boolean>(),
-    inset: optional<Spacing | Properties['inset'] | boolean | number>(),
-    top: optional<Spacing | Properties['top'] | boolean | number>(),
-    left: optional<Spacing | Properties['left'] | boolean | number>(),
-    right: optional<Spacing | Properties['right'] | boolean | number>(),
-    bottom: optional<Spacing | Properties['bottom'] | boolean | number>(),
-    z: optional<Level | Properties['zIndex'] | number>(),
+    ...responsive({
+        position: optional<Properties['position']>('relative'),
+        relative: optional<boolean>(),
+        absolute: optional<boolean>(),
+        fixed: optional<boolean>(),
+        sticky: optional<boolean>(),
+        static: optional<boolean>(),
+        inset: optional<Spacing | Properties['inset'] | boolean | number>(),
+        top: optional<Spacing | Properties['top'] | boolean | number>(),
+        left: optional<Spacing | Properties['left'] | boolean | number>(),
+        right: optional<Spacing | Properties['right'] | boolean | number>(),
+        bottom: optional<Spacing | Properties['bottom'] | boolean | number>(),
+        z: optional<Level | Properties['zIndex'] | number>(),
+    }),
 }))
 
 export type PositionPropsDefinition = typeof positionPropsDefinition
